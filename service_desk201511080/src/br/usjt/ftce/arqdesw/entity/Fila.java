9567403c309e972a9645fd6b7193bbbc9bd2cf40
Naldo <Naldo@Naldo>
2018-02-23 18:32:08 -0300

    ARQUITETURA DE SOFTWARE

---
+++ service_desk201511080/src/br/usjt/ftce/arqdesw/entity/Fila.java
@@ -0,0 +1,23 @@
+package br.usjt.ftce.arqdesw.entity;
+
+public class Fila {
+	
+	int id;
+	String nome;
+	
+	public int getId(){
+		return id;
+	}
+	public void setId(int id){
+		this.id = id;
+	}
+	public String getNome(){
+		return nome;
+	}
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+	
+	
+
+}
